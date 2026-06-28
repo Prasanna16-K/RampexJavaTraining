d842ed5c74610a1bef7c1444925afdb47211a06b
PRASANNA K <prasanna161616@gmail.com>
2026-06-28 22:00:19 +0530

    code created

--- src/Day4/Multiply.java
+++ src/Day4/Multiply.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-class Multiply{
-    public static void main(String[] args){
-        int multiply=1;
+class Multiply {
+    public static void main(String[] args) {
+        int multiply = 1;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number: ");
         int n = sc.nextInt();
-        for(int i = 1; i <= n; i++) {
-            multiply = multiply*i;
+        for (int i = 1; i <= n; i++) {
+            multiply = multiply * i;
             System.out.println(multiply);
         }
     }
