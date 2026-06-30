3ab43803dcbe77e916119146abb73323a083eee3
PRASANNA K <prasanna161616@gmail.com>
2026-06-30 10:36:20 +0530

    code created

---
+++ src/Day6/classtask/NestedLoop.java
@@ -0,0 +1,12 @@
+package Day6.classtask;
+
+public class NestedLoop {
+    public static void main(String[] args){
+        for(int i = 1;i<=5;i++){
+            for(int j = 5;j>=0;j--){
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ src/Day7/classtask/ArraySecondMax.java
@@ -0,0 +1,16 @@
+package Day7.classtask;
+
+public class ArraySecondMax {
+    public static void main(String[] args){
+        int nums[]={1,9,17,8,5};
+        int Smax=0;
+        int max=0;
+        for (int i=0;i<nums.length;i++){
+            if(nums[i]>max){
+                Smax=max;
+                max=nums[i];
+            }
+        }
+        System.out.println("the second maximun value: "+Smax);
+    }
+}
